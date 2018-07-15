3603b749870bd597dd0bbe2158da8c33f9abd130
ZITOUNI Mohamed <zitouni.mohamed.email@gmail.com>
2018-07-15 16:00:17 +0200

    finir le crud component

--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -4,80 +4,44 @@ import { ActivatedRoute } from '@angular/router';
 
 import {ProduitService} from './produit.service';
 import {Produit} from '../shared/produit.model';
+import { DataModel } from '../shared/data.model';
 
 @Component({
   selector: 'app-produit',
   templateUrl: './produit.component.html',
   styleUrls: ['./produit.component.css']
 })
-export class ProduitComponent implements OnInit{
+export class ProduitComponent implements OnInit {
 
-  public produits: Produit[];
+  produits: Produit[];
 
   produitForm: FormGroup;
 
-  operation: string = 'add';
+  produit: Produit = new Produit();
 
-  selectedProduit: Produit;
+  produitsModel: DataModel[];
 
   constructor(private produitService: ProduitService, private fb: FormBuilder, private route: ActivatedRoute){
-    this.createForm();
+
   }
 
   ngOnInit(){
-    this.initProduit();
     this.produits = this.route.snapshot.data.produits;
-  }
-
-  createForm(){
     this.produitForm = this.fb.group({
       ref: ['', Validators.required],
       quantite: '',
       prixUnitaire: ''
     });
-  }
-  loadProduits(){
-    this.produitService.getAll().subscribe(
-      data => {this.produits = data},
-      error => { console.log('An error was occured.')},
-      () => { console.log('loading produits was done.')}
-    );
+
+    this.produitsModel = [
+      new DataModel('id','ID','number',true,[]),
+      new DataModel('ref','Référence','string',false,[]),
+      new DataModel('quantite','Quantité','number',false,[]),
+      new DataModel('prixUnitaire','Prix Unitaire','number',false,[])
+    ]
   }
 
-  addProduit(){
-    const p = this.produitForm.value;
-    this.produitService.add(p).subscribe(
-      res => {
-        this.initProduit();
-        this.loadProduits();
-      }
-    );
-  }
 
-  updateProduit(){
-    this.produitService.update(this.selectedProduit)
-    .subscribe(
-      res => {
-        this.initProduit();
-        this.loadProduits();
-      }
-    );
-  }
-
-  initProduit(){
-    this.selectedProduit = new Produit();
-    this.createForm();
-  }
-
-  deleteProduit(){
-    this.produitService.delete(this.selectedProduit.id).
-    subscribe(
-      res =>{
-        this.selectedProduit = new Produit();
-        this.loadProduits();
-      }
-    );
-  }
 
 
 
---
+++ src/app/shared/data.model.ts
@@ -0,0 +1,8 @@
+
+export class DataModel {
+  constructor(public columnName?: string,
+              public columnReference?: string,
+              public dataType: string,
+              public readonly?: boolean,
+              public messages?: any){}
+}
