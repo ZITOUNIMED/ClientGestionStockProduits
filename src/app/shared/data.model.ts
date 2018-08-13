c5db3be74d624c47efc77cb9a5951474244d3b20
ZITOUNI Mohamed <zitouni.mohamed.email@gmail.com>
2018-08-13 20:53:52 +0200

    petit fix sur tuto24

--- src/app/shared/data.model.ts
+++ src/app/shared/data.model.ts
@@ -2,7 +2,7 @@
 export class DataModel {
   constructor(public columnName?: string,
               public columnReference?: string,
-              public dataType: string,
+              public dataType?: string,
               public readonly?: boolean,
               public messages?: any){}
 }
