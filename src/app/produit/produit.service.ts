2abc38ad70e24f2ed214c669193712b19792fd5d
ZITOUNI Mohamed <zitouni.mohamed.email@gmail.com>
2018-12-30 18:15:45 +0100

    refacto crud service

--- src/app/produit/produit.service.ts
+++ src/app/produit/produit.service.ts
@@ -1,39 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
 
-import { API_URLS} from '../config/api.url.config';
-import { Produit } from '../shared/produit.model';
+import { API_URLS } from '../config/api.url.config';
 import { CrudService } from '../shared/crud.service';
 
 @Injectable()
-export class ProduitService implements CrudService{
-
-  constructor(private http: HttpClient){
-
-  }
-
-  getAll(): Observable<any>{
-    return this.http.get(API_URLS.PRODUITS_URL);
-  }
-
-  add(produit): Observable<any>{
-    return this.http.post(API_URLS.PRODUITS_URL, produit);
-  }
-
-  update(produit): Observable<any>{
-    return this.http.put(API_URLS.PRODUITS_URL, produit);
-  }
-
-  delete(id): Observable<any>{
-    return this.http.delete(API_URLS.PRODUITS_URL + `/${id}`);
-  }
-
-  addAll(list): Observable<any>{
-    return this.http.post(API_URLS.PRODUITS_URL + '/all', list);
-  }
-
-
-
-
+export class ProduitService extends CrudService{
+  url = API_URLS.PRODUITS_URL;
 }
